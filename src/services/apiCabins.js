7ea9a482e57e1aef91f270dcd811286263a6f570
Samuel-Toriola <samueloladipupo.toriola@gmail.com>
2024-04-04 11:29:05 +0100

    Integrating Supabase and adding react-hot-toast

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,24 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+
+  if (error) {
+    console.error();
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error();
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
+}
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,6 +15,14 @@ const Main = styled.main`
   padding: 4rem 4rem 6.4rem;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledApplayout>
@@ -23,7 +31,9 @@ function AppLayout() {
       {/* <header>HEADER</header>
       <aside>SIDEBAR</aside> */}
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledApplayout>
   );
